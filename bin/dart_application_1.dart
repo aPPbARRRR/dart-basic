fa72d6f4f659ca82d3e8b8134fe34778142a59d6
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-11-27 17:37:08 +0900

    fix: test fix

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,5 +1,5 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+  print('Hello world');
 }
