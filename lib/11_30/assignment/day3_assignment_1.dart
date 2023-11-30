721e3470f4d7fe76db27f8631e439e38c0c5c568
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-11-30 16:51:02 +0900

    [assignment] modify: add cleric class action

--- lib/11_30/assignment/day3_assignment_1.dart
+++ lib/11_30/assignment/day3_assignment_1.dart
@@ -1,19 +1,33 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Cleric {
 
+    Cleric({
+    required this.name,
+    required this.curHp,
+    required this.curMp,
+  });
+
   final String name;
   int curHp;
   int curMp;
   static const int maxHp = 50;
   static const int maxMp = 10;
 
-  Cleric({
-    required this.name,
-    required this.curHp,
-    required this.curMp,
-  });
+  void selfAid () {
+    if(curMp<5) return;
+    curMp-5;
+    curHp = maxHp;
+  }
+
+  @override
+  String toString() => 'name: ${this.name}, hp: ${this.curHp}, mp: ${this.curMp}';
+
 }
 
 void main() {
   Cleric cleric = Cleric(name: 'adf', curHp: 10, curMp: 20);
+  print(cleric.toString());
+
+  cleric.selfAid();
+  print(cleric.toString());
 }
