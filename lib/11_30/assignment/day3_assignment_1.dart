f287b58066f94044cf4b5a38d282e4880b3c9db3
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-11-30 16:41:18 +0900

    [assignment] add: add cleric class property

--- lib/11_30/assignment/day3_assignment_1.dart
+++ lib/11_30/assignment/day3_assignment_1.dart
@@ -1,1 +1,19 @@
-class Cleric {}
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Cleric {
+
+  final String name;
+  int curHp;
+  int curMp;
+  final int maxHp = 50;
+  final int maxMp = 10;
+
+  Cleric({
+    required this.name,
+    required this.curHp,
+    required this.curMp,
+  });
+}
+
+void main() {
+  Cleric cleric = Cleric(name: 'adf', curHp: 10, curMp: 20);
+}
