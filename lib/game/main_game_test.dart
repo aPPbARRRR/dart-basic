756ec06a3ca76d61a1f08360ac1785e7b0bcb47c
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-12-04 15:18:20 +0900

    연습문제 10-2 수정 : setter 추가하여 mp값 오류 방지

--- lib/game/main_game_test.dart
+++ lib/game/main_game_test.dart
@@ -1,11 +1,9 @@
-
 import 'package:dart_application_1/game/wand.dart';
 import 'package:dart_application_1/game/wizard.dart';
 
 import 'cleric.dart';
 
 void main() {
-
   // Cleric asus = Cleric(name: 'asus', curHp: 10, curMp: 3);
   // Cleric mac = Cleric(name: 'mac');
   // Cleric window = Cleric(name: 'window', curHp: 20);
@@ -20,13 +18,19 @@ void main() {
   // print(asus.toString());
 
   try {
-  Wizard wizard1 = Wizard(name: 'asdgf', hp: -18, mp: 10, wand: Wand(name: 'wand', power: 100));
-  print(wizard1.info); }
-  catch (e) {print(e);}
+    Wizard wizard1 = Wizard(
+        name: 'asdgf', hp: 10, mp: 5, wand: Wand(name: 'wand', power: 100));
+    print(wizard1.info);
+    try {
+      wizard1.mp = -3;
+    } catch (e) {
+      print(e);
+    }
+    print(wizard1.info);
+  } catch (e) {
+    print(e);
+  }
 
-     Map list = {'1' : 1, '2' : 2, '3' : 3};
-    print(list.keys.toList());
-
+  //  Map list = {'1' : 1, '2' : 2, '3' : 3};
+  // print(list.keys.toList());
 }
-
-
--- lib/game/wizard.dart
+++ lib/game/wizard.dart
@@ -4,24 +4,25 @@ import 'wand.dart';
 class Wizard {
   final String name;
   int hp;
-  int mp;
+  int? _mp;
   Wand? wand;
 
-  void validation() {}
+  void set mp (int mp) => this._mp = mp < 0 ? throw Exception('wizard의 mp는 0보다 작을 수 없습니다.'): _mp = mp; 
 
   Wizard({
     required this.name,
     required this.hp,
-    required this.mp,
+    required int mp,
     this.wand,
   }) {
+    this._mp = mp;
     if (name.length < 3) throw Exception('Wizard의 이름이 제대로 설정되지 않았습니다.');
     if( wand == null ) throw Exception('Wand가 없습니다.');
     if( mp <= 0) throw Exception('Wizard의 mp가 제대로 설정되지 않았습니다.');
     if( hp < 0 ) this.hp = 0;
   }
 
-  String get info => 'name: $name, hp/mp: $hp/$mp, wand: ${wand!.name}/${wand!.power}';
+  String get info => 'name: $name, hp/mp: $hp/$_mp, wand: ${wand!.name}/${wand!.power}';
 }
 
 
