f38a24b34bfb516d76de853d445108c7544feae3
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-12-04 15:26:45 +0900

    연습문제 10-2 수정 : Wand final 추가하여 오류 방지

--- lib/game/wizard.dart
+++ lib/game/wizard.dart
@@ -7,7 +7,8 @@ class Wizard {
   int? _mp;
   Wand? wand;
 
-  void set mp (int mp) => this._mp = mp < 0 ? throw Exception('wizard의 mp는 0보다 작을 수 없습니다.'): _mp = mp; 
+  void set mp(int mp) => this._mp =
+      mp < 0 ? throw Exception('wizard의 mp는 0보다 작을 수 없습니다.') : _mp = mp;
 
   Wizard({
     required this.name,
@@ -17,12 +18,11 @@ class Wizard {
   }) {
     this._mp = mp;
     if (name.length < 3) throw Exception('Wizard의 이름이 제대로 설정되지 않았습니다.');
-    if( wand == null ) throw Exception('Wand가 없습니다.');
-    if( mp <= 0) throw Exception('Wizard의 mp가 제대로 설정되지 않았습니다.');
-    if( hp < 0 ) this.hp = 0;
+    if (wand == null) throw Exception('Wand가 없습니다.');
+    if (mp <= 0) throw Exception('Wizard의 mp가 제대로 설정되지 않았습니다.');
+    if (hp < 0) this.hp = 0;
   }
 
-  String get info => 'name: $name, hp/mp: $hp/$_mp, wand: ${wand!.name}/${wand!.power}';
+  String get info =>
+      'name: $name, hp/mp: $hp/$_mp, wand: ${wand!.name}/${wand!.power}';
 }
-
-
