ecc0d89c65826da7952ed9ca75a9ec2a0c6620ce
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-12-05 16:24:47 +0900

    day7 연습문제 12 -1 제출용

---
+++ lib/12_05/assignment_12_1/book.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Book {
+  String name;
+  int price;
+  String color;
+  String isbn;
+  
+  Book({
+    required this.name,
+    required this.price,
+    required this.color,
+    required this.isbn,
+  });
+}
---
+++ lib/12_05/assignment_12_1/computer.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Computer {
+  String name;
+  int price;
+  String color;
+  String makerName;
+
+  Computer({
+    required this.name,
+    required this.price,
+    required this.color,
+    required this.makerName,
+  });
+}
