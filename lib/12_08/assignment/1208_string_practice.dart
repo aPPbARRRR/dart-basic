5c92fbb15d8199989e7e438cb6cb276c0a8283d3
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-12-08 20:55:47 +0900

    strongbox 문제, 문자열다루기 문제

---
+++ lib/12_08/assignment/1208_string_practice.dart
@@ -0,0 +1,23 @@
+class Word {
+  String sord = '';
+// 모음이면 true 반환하도록 작성하시오.
+  bool isVowel(int i) => 0 <= i && i < sord.length
+      ? 'aeiouAEIOU'.contains(sord[i])
+      : throw Exception('범위를 벗어난 입력입니다.');
+
+// 자음이면 bool값을 반환하시오.
+  bool isConsonant(int i){
+    String consonants = 'bcdfghjklmnpqrstvwxyz';
+    consonants = consonants+consonants.toUpperCase();
+    return consonants.contains(sord[i]);
+  }
+}
+
+void main() {
+  Word word = Word();
+  word.sord = 'ababaAFGD';
+
+  print(word.isVowel(5)); // A - true
+  print(word.isConsonant(5)); // A - false
+}
+
---
+++ lib/12_08/assignment/1208_strong_box.dart
@@ -0,0 +1,40 @@
+enum KeyType { padlock, button, dial, finger }
+
+class StrongBox<T> {
+  StrongBox({required this.keyType});
+
+  T? _instance;
+  KeyType keyType;
+  int _count = 0;
+
+  void put(T instance) => _instance = instance;
+
+  //test
+  get count => _count;
+
+  T? get() {
+    T? result = null;
+    switch (keyType) {
+      case KeyType.padlock:
+        _count < 1024 ? _count++ : result = _instance;
+      case KeyType.button:
+        _count < 10000 ? _count++ : result = _instance;
+      case KeyType.dial:
+        _count < 30000 ? _count++ : result = _instance;
+      case KeyType.finger:
+        _count < 1000000 ? _count++ : result = _instance;
+    }
+    return result;
+  }
+}
+
+void main() {
+  StrongBox<String> box = StrongBox(keyType: KeyType.button);
+  box.put('ㅁㄴㅇㄹ');
+
+  for (int i =1; i < 10005; i++) {
+    print('${box.count +1}번째 시도');
+    print(box.get());
+  }
+}
+
