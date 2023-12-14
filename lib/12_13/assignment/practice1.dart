d58fa5e92ad2c6d1b2f5fb6a66398ee1995ff5ac
jun <134025007+aPPbARRRR@users.noreply.github.com>
2023-12-14 09:28:30 +0900

    과제1 수정 -> 카피파일 새로 생성하는것으로 변경

--- lib/12_13/assignment/practice1.dart
+++ lib/12_13/assignment/practice1.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 void modifyCSV() async {
   try {
     File target = File('lib/12_13/assignment/sample.csv');
-    await target
-        .readAsString()
-        .then((value) => target.writeAsString(value.replaceAll('한석봉', '김석봉')));
+    if (target.readAsStringSync().contains('한석봉'))
+      await target
+          .readAsString()
+          .then((value) => target.writeAsString(value.replaceAll('한석봉', '김석봉')))
+        ..copy('lib/12_13/assignment/sample_copy.csv');
   } catch (e) {
     print(e);
   }
